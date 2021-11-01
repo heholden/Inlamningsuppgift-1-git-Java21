945a969a4d27e6c83a94c671102ced4c7fd39428
hannah holden <heholden@gmail.com>
2021-11-01 10:12:29 +0100

    change path run_game.sh

--- run_game.sh
+++ run_game.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 chmod u+x run_game.sh
+PATH1=https://github.com/heholden/Inlamningsuppgift-1-git-Java21/blob/main/Guesser.java
+PATH2=https://github.com/heholden/Inlamningsuppgift-1-git-Java21/blob/main/GuessingGame.java
 echo "Hannah Holden"
-mkdir Hannah_Holden_labb
-cp Guesser.java Hannah_Holden_labb
-cp GuessingGame.java Hannah_Holden_labb
-cd $Hannah_Holden_labb
+mkdir -m777 Hannah_Holden_labb 
+cp $PATH1 $PATH2 Hannah_Holden_labb/
+cd Hannah_Holden_labb
 pwd
 echo "compiling..."
 javac Guesser.java
 javac GuessingGame.java
 echo "running game..."
-java GuessingGame.java
+java GuessingGame
 echo "done"
 rm Guesser.class
 rm GuessingGame.class
